985de2e7b41bfe6108b8f318e167e6f5aa7070cd
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2019-10-30 12:07:04 +0000

    Rename server.js to index.js; use mongoose to connect to database

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = process.env.PORT || 5000
+
+
 app.listen(PORT, () => console.log('Server has started'));
 
 app.get('/',(req, res) => res.send('Hello'));
+
+mongoose.connect('mongodb://localhost/bestB4', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+  .then(() => {
+    console.log("We have connected")
+  });
