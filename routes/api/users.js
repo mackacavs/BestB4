eeb1172bbe7d23e3b4205c90dbf9ec30ffb63c0c
etelish <etelisha@googlemail.com>
2019-10-30 15:00:30 +0000

    created validation checker for user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,20 +1,39 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check')
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
 
 router.post('/', [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
   ],
-    (req, res) => {
+    async (req, res) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
       }
-        res.send('Body was sent successfully')
+        const { name, email, password } = req.body
+        try{ let user = await User.findOne({ email: email})
+        if (user) {
+          return res.status(400).json({ errors: [{ msg: 'User already exists'}] })
+        }
+        const avatar = gravatar.url(email)
+        user = new User ({
+          name,
+          email,
+          password,
+          avatar
+        })
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        await user.save();
+        }catch (err){
+             }
+
       })
 
 module.exports =router;
-
-
