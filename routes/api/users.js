8e1da1f001040bf758561b8c941a11f25eed45e2
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2019-10-30 15:25:50 +0000

    Use jwt to create a token for a user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,11 @@
+const config = require('config');
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check')
 const User = require('../../models/User');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 
 router.post('/', [
@@ -31,7 +33,26 @@ router.post('/', [
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(password, salt);
         await user.save();
+
+        const payload = {
+          user: {
+            id: user.id
+          }
+        };
+
+        jwt.sign(
+          payload,
+          config.get('jwtToken'),
+          { expiresIn: 360000 },
+          (err, token) => {
+            if (err) throw error;
+            res.json({ token });
+          }
+        )
+
         }catch (err){
+          cosole.error(err.message)
+          res.status(500).send('Server Error')
              }
 
       })
