3b56af2b37144e759d41bd42a6eda062e2f9e0f5
Valeria Serci <balerty84@gmail.com>
2019-10-31 10:37:50 +0000

    Add Post model

---
+++ models/Post.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema ({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    expiry: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = Post = mongoose.model('post', postSchema);
