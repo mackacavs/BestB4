4d5e920b21a3a2cb5648b1bd511c8a385bbd8df0
mackacavs <mackenziegdixon@yahoo.com>
2019-11-04 11:38:00 +0000

    Have first failing test

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const app = express();
-const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000
 
 app.use(express.json({ extended: false }))
 
-app.listen(PORT, () => console.log('Server has started'));
+// app.listen(PORT, () => console.log('Server has started'));
 
-app.get('/',(req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.send('Hello'));
 
-mongoose.connect('mongodb://localhost/bestB4', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-})
-  .then(() => {
-    console.log("We have connected")
-  });
+if (process.env.NODE_ENV = 'test') {
+  console.log("We are here")
+  mongoose.connect('mongodb://localhost/testDatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+    .catch((err) => console.log(err));
+} else {
+  mongoose.connect('mongodb://localhost/bestB4', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+    .catch((err) => console.log(err));
+
+}
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/login', require('./routes/api/login'));
+
+module.exports = app
+
+  // .then(() => {
+  // console.log("We have connected")
+  // })
---
+++ test/demo.spec.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const app = require('../index');
+const mongoose = require('mongoose');
+
+describe('Test the root path', () => {
+
+  // beforeAll(() => {
+  //   mongoose.connect('mongodb://localhost/bestB4', {
+  //     useNewUrlParser: true,
+  //     useUnifiedTopology: true,
+  //     useFindAndModify: false
+  //   })
+  // });
+
+  test('It should respond to the GET method', (done) => {
+    request(app).get('/').then((response) => {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+    done();
+
+  });
+
+  afterAll((done) => {
+    mongoose.connection.close()
+  });
+});
